6cdfeefd31b07722e07861afaff2640f4880e433
Anand <hr.anand0101@gmail.com>
2023-06-24 18:37:06 +0530

    adding add changes:)

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
+import React, { useState } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { PhotoFrame } from "./PhotoFrame";
+
 const App = () => {
-  
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [photo, setPhoto] = useState({});
+  const BASE_URL = "https://jsonplaceholder.typicode.com/photos/";
 
+  const fetchApi = async (id) => {
+    if (id == 0) {
+      setPhoto({});
+      return;
+    }
+    setIsLoading(true);
+    const response = await fetch(BASE_URL + id);
+    const data = await response.json();
+    setIsLoading(false);
+    setPhoto(data);
+  };
+  function handleChange(event) {
+    let value = event.target.value;
+    fetchApi(value);
+  }
 
+  return (
+    <div id="main">
+      <label htmlFor="input">Id number</label>
+      <input id="input" type="number" onChange={handleChange} />
+      {isLoading && <Loader />}
+      {photo.id > 0 && <PhotoFrame {...photo} />}
+    </div>
+  );
+};
 export default App;
